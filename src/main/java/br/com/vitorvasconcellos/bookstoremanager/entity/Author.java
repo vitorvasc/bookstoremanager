5b62e11f7143b3662b21c34e632e215c22b54fd3
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2021-06-08 00:54:26 -0300

    Added entities

---
+++ src/main/java/br/com/vitorvasconcellos/bookstoremanager/entity/Author.java
@@ -0,0 +1,17 @@
+package br.com.vitorvasconcellos.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
