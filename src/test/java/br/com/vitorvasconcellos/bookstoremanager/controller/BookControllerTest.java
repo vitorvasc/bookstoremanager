8c28c9f26d81f45872713b82acbb602128839518
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2021-06-09 19:13:47 -0300

    Added test class

---
+++ src/test/java/br/com/vitorvasconcellos/bookstoremanager/controller/BookControllerTest.java
@@ -0,0 +1,72 @@
+package br.com.vitorvasconcellos.bookstoremanager.controller;
+
+import br.com.vitorvasconcellos.bookstoremanager.dto.BookDTO;
+import br.com.vitorvasconcellos.bookstoremanager.dto.MessageResponseDTO;
+import br.com.vitorvasconcellos.bookstoremanager.service.BookService;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static br.com.vitorvasconcellos.bookstoremanager.utils.BookUtils.*;
+import static br.com.vitorvasconcellos.bookstoremanager.utils.BookUtils.createFakeBookDTO;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookControllerTest {
+
+    public static final String BOOK_API_URL_PATH = "/api/v1/books";
+    private MockMvc mockMvc;
+
+    @Mock
+    private BookService bookService;
+
+    @InjectMocks
+    private BookController bookController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(bookController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void testWhenPOSTisCalledThenABookShouldBeCreated() throws Exception {
+        BookDTO bookDTO = createFakeBookDTO();
+        MessageResponseDTO expectedMessageResponse = MessageResponseDTO.builder()
+                .message("Book created with ID " + bookDTO.getId())
+                .build();
+
+        when(bookService.create(bookDTO))
+                .thenReturn(expectedMessageResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BOOK_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(bookDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", Is.is(expectedMessageResponse.getMessage())));
+    }
+
+    @Test
+    void testWhenPOSTwithInvalidISBNIsCalledThenBadRequestShouldBeReturned() throws Exception {
+        BookDTO bookDTO = createFakeBookDTO();
+        bookDTO.setIsbn("invalid isbn");
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BOOK_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(bookDTO)))
+                .andExpect(status().isBadRequest());
+    }
+}
---
+++ src/test/java/br/com/vitorvasconcellos/bookstoremanager/utils/AuthorUtils.java
@@ -0,0 +1,34 @@
+package br.com.vitorvasconcellos.bookstoremanager.utils;
+
+import br.com.vitorvasconcellos.bookstoremanager.dto.AuthorDTO;
+import br.com.vitorvasconcellos.bookstoremanager.entity.Author;
+import com.github.javafaker.Faker;
+
+public class AuthorUtils {
+
+    private static final Faker faker = Faker.instance();
+
+    public static AuthorDTO createFakeAuthorDTO() {
+        return AuthorDTO.builder()
+                .id(faker.number().randomNumber())
+                .name(faker.book().author())
+                .age(faker.number().numberBetween(0, 100))
+                .build();
+    }
+
+    public static Author createFakeAuthor() {
+        return Author.builder()
+                .id(faker.number().randomNumber())
+                .name(faker.book().author())
+                .age(faker.number().numberBetween(0, 100))
+                .build();
+    }
+
+    public static Author createFakeAuthorFrom(AuthorDTO authorDTO) {
+        return Author.builder()
+                .id(authorDTO.getId())
+                .name(authorDTO.getName())
+                .age(authorDTO.getAge())
+                .build();
+    }
+}
