de80dec181230e490e8cc17a176c9f3432c72517
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2021-06-09 19:30:28 -0300

    Added endpoint to retrieve by id

--- src/main/java/br/com/vitorvasconcellos/bookstoremanager/controller/BookController.java
+++ src/main/java/br/com/vitorvasconcellos/bookstoremanager/controller/BookController.java
@@ -4,6 +4,8 @@ import br.com.vitorvasconcellos.bookstoremanager.dto.BookDTO;
 import br.com.vitorvasconcellos.bookstoremanager.dto.MessageResponseDTO;
 import br.com.vitorvasconcellos.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,10 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id) {
+        return bookService.findById(id);
     }
 }
--- src/main/java/br/com/vitorvasconcellos/bookstoremanager/service/BookService.java
+++ src/main/java/br/com/vitorvasconcellos/bookstoremanager/service/BookService.java
@@ -8,6 +8,8 @@ import br.com.vitorvasconcellos.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
     private BookRepository bookRepository;
@@ -26,5 +28,10 @@ public class BookService {
         return MessageResponseDTO.builder()
                 .message("Book created with ID " + savedBook.getId())
                 .build();
+    }
+
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
     }
 }
