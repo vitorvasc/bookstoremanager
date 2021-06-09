6d78cd5206ba138a20fd72de4426c2784938bc7e
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2021-06-09 19:13:18 -0300

    Removed unused imports and added @Valid annotation

--- src/main/java/br/com/vitorvasconcellos/bookstoremanager/controller/BookController.java
+++ src/main/java/br/com/vitorvasconcellos/bookstoremanager/controller/BookController.java
@@ -1,14 +1,15 @@
 package br.com.vitorvasconcellos.bookstoremanager.controller;
 
+import br.com.vitorvasconcellos.bookstoremanager.dto.BookDTO;
 import br.com.vitorvasconcellos.bookstoremanager.dto.MessageResponseDTO;
-import br.com.vitorvasconcellos.bookstoremanager.entity.Book;
-import br.com.vitorvasconcellos.bookstoremanager.repository.BookRepository;
 import br.com.vitorvasconcellos.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -22,7 +23,7 @@ public class BookController {
     }
 
     @PostMapping
-    public MessageResponseDTO create(@RequestBody Book book) {
-        return bookService.create(book);
+    public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
+        return bookService.create(bookDTO);
     }
 }
