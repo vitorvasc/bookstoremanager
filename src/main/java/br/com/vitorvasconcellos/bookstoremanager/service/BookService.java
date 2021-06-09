c799812f0613a91bb8c4d3687696c343fe61f7b2
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2021-06-09 19:13:31 -0300

    Added mapper to Service class

--- src/main/java/br/com/vitorvasconcellos/bookstoremanager/service/BookService.java
+++ src/main/java/br/com/vitorvasconcellos/bookstoremanager/service/BookService.java
@@ -1,23 +1,28 @@
 package br.com.vitorvasconcellos.bookstoremanager.service;
 
+import br.com.vitorvasconcellos.bookstoremanager.dto.BookDTO;
 import br.com.vitorvasconcellos.bookstoremanager.dto.MessageResponseDTO;
 import br.com.vitorvasconcellos.bookstoremanager.entity.Book;
+import br.com.vitorvasconcellos.bookstoremanager.mapper.BookMapper;
 import br.com.vitorvasconcellos.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class BookService {
     private BookRepository bookRepository;
+
+    private final BookMapper bookMapper = BookMapper.INSTANCE;
 
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public MessageResponseDTO create(Book book) {
-        Book savedBook = bookRepository.save(book);
+    public MessageResponseDTO create(BookDTO bookDTO) {
+        Book BookToSave = bookMapper.toModel(bookDTO);
+
+        Book savedBook = bookRepository.save(BookToSave);
         return MessageResponseDTO.builder()
                 .message("Book created with ID " + savedBook.getId())
                 .build();
